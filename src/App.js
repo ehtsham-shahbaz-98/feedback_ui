97bb5e11efa4866fb025354e121a2ac3c331736e
Ehtsham Shahbaz <ehtsham.shahbaz.1998@gmail.com>
2022-10-06 08:09:05 -0700

    Feedback Stats Added

--- src/App.js
+++ src/App.js
@@ -2,19 +2,29 @@ import React from "react";
 
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
+import FeedbackStats from "./components/FeedbackStats";
+
 import FeedbackData from "./data/FeedbackData";
+import Card from "./components/shared/Card";
 
 function App() {
-    const [feedback, setFeedback] = React.useState(FeedbackData);
+  const [feedback, setFeedback] = React.useState(FeedbackData);
 
-    return (
-        <>
-            <Header text="Feedback UI" />
-            <div className="container">
-                <FeedbackList feedback={feedback} />
-            </div>
-        </>
-    );
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  return (
+    <>
+      <Header text="Feedback UI" />
+      <div className="container">
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
+  );
 }
 
 export default App;
