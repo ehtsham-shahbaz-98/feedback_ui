f79ddcaf585b4e3d7550cde78f86e6a61e470333
ehtsham-shahbaz-98 <ehtsham.shahbaz.1998@gmail.com>
2022-10-24 08:32:42 -0700

    Edit Functionlity Done

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -41,6 +41,11 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback([newFeedback, ...feedback]);
   };
 
+  // Update feedback item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(feedback.map((item) => item.id === id ? { ...item, ...updItem } : item));
+  }
+
   // Set item to be updated
   const editFeedback = (item) => {
     setFeedbackEdit({
@@ -53,9 +58,11 @@ export const FeedbackProvider = ({ children }) => {
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
-        editFeedback
+        editFeedback,
+        updateFeedback
       }}
     >
       {children}
